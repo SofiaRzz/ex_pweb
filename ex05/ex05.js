57736c0eae9304308c90467feb3c73a72a4ce664
SofiaRzz <sofiarizzottobrasil@gmail.com>
2023-03-22 17:51:34 -0300

    function validaData()

--- ex05/ex05.js
+++ ex05/ex05.js
@@ -1,18 +1,38 @@
-function validaData(){
-  var dataResposta = document.getElementById("data")
-  var dataSplit = dataResposta.value.split("-")
+var resultado = document.getElementById('invalido')
+function validaData() {
+  var dataResposta = document.getElementById('data')
+  var dataSplit = dataResposta.value.split('-')
   var diaInput = parseInt(dataSplit[2])
   var mesInput = parseInt(dataSplit[1])
   var anoInput = parseInt(dataSplit[0])
 
   var dataAtual = new Date()
   var diaAtual = dataAtual.getDate()
-  var mesAtual = dataAtual.getMonth()
+  var mesAtual = dataAtual.getMonth() + 1
   var anoAtual = dataAtual.getFullYear()
+
+  if (anoInput > anoAtual) {
+    resultado.innerHTML = 'Data de nascimento inválida'
+    resultado.style.display = 'block'
+    return false
+  } else if (anoInput == anoAtual && mesInput > mesAtual) {
+    resultado.innerHTML = 'Data de nascimento inválida'
+    resultado.style.display = 'block'
+    return false
+  } else if (
+    anoInput == anoAtual &&
+    mesInput == mesAtual &&
+    diaInput > diaAtual
+  ) {
+    resultado.innerHTML = 'Data de nascimento inválida'
+    resultado.style.display = 'block'
+    return false
+  }
+  return dataResposta
 }
 
-
-
-function enviaFormulario(){
-
+function enviaFormulario() {
+  if (!validaData()) {
+    return
+  }
 }
