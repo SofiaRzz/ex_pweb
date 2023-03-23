862b28d24ab088346b04ff98dfe5657d1a8ace2a
SOFIA PACHECO RIZZOTTO <2021321147@IFAL-MACEIO.INTRANET>
2023-03-23 14:42:20 -0300

    finalizando ex05

--- ex05/ex05.js
+++ ex05/ex05.js
@@ -31,8 +31,83 @@ function validaData() {
   return dataResposta
 }
 
-function enviaFormulario() {
-  if (!validaData()) {
+function validaDDD(){
+  var dddInput = document.getElementById("ddd").value
+  const dddsBrasil = [
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '21', '22', '24','27', '28','31', '32', '33', '34', '35', '37', '38','41', '42', '43', '44', '45', '46','47', '48', '49','51', '53', '54', '55','61','62', '64','63','65', '66','67','68','69','71', '73', '74', '75', '77','79','81', '87','82','83','84','85', '88','86', '89','91', '93', '94','92','95', '96', '97', '98', '99'
+  ];
+
+  for (var i = 0; i < dddsBrasil.length; i++){
+    if (dddInput == dddsBrasil[i]){
+      return true
+    }
+  }
+  resultado.style.display = "block"
+  resultado.innerHTML = "DDD inválido" 
+  return false
+}
+
+function validaTelefone(){
+  var telefone = document.getElementById("telefone").value
+  if (telefone.length > 9 || telefone.length < 8) {
+    resultado.style.display = 'block'
+    resultado.innerHTML = "Telefone inválido"
+    return false
+  }
+  return true
+
+}
+
+function validaEmail() {
+  var email = document.getElementById("email").value.split("@")[1]
+  for (var i = 0; i < email.length; i++){
+    if (email[i] == "."){
+      return true
+    }
+  }
+  resultado.style.display = "block"
+  resultado.innerHTML = "E-mail precisa de um domínio" 
+  return false
+}
+
+function validaCheckbox(){
+  var checkExtras = document.getElementsByName("checkExtras")
+  var contador = 0;
+  for (var i = 0; i < checkExtras.length; i++){
+    if (checkExtras[i].checked == true){
+      contador++
+    }
+  }
+  if (contador > 3){
+    resultado.style.display = "block"
+    resultado.innerHTML = "Você só pode escolher três atividades extracurriculares" 
+    return false
+  }
+}
+
+function enviaFormulario(e) {
+  e.preventDefault()
+  resultado.style.display = 'none'
+
+  if (validaData() == false) {
     return
   }
+
+  if (validaDDD() == false) {
+    return
+  }
+
+  if (validaTelefone() == false) {
+    return
+  }
+
+  if (validaEmail() == false) {
+    return
+  }
+
+  if (validaCheckbox() == false){
+    return
+  }
+
+  alert("Formulário enviado com sucesso!")
 }
